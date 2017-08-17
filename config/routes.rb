e28e2d2eb78c2f9b1e26c519f3dcdd8d9aa722e0
thunderenlight <thunderenlight@gmail.com>
2017-08-17 12:54:14 -0500

    adding dev default

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,4 @@
 # end
 
 
-hsh = {"a"=> 21, "b" => true, "c"=> 23}
-
-def optimize(hsh)   
-	hsh.reduce({}) do |new_hsh, (k,v)| 	
-	new_hsh[k.to_sym] = v.kind_of?(Hash) ? optimize(v) : v 	
-	new_hsh   
-	end 
-end
-
-p optimize(hsh)
-
-def flat(arr)
-  array.each_with_object([]) do |el, flat_arr|
-    flat_arr << (el.is_a?(Array) ? flat(el) : el)
-  end
-end
-
-p flat([1,2,3,4,[1,2,3,4],5])
+#  []
